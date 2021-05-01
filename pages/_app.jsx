ecbc3bb53390bf3da0ed7b8a23d5310d1c244997
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-01 10:36:52 +0300

    Init next example

---
+++ pages/_app.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    const jssStyles = document.getElementById("mantine-ssr-styles");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Mantine next example</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import TransactionTable from "../components/TransactionsTable/TransactionTable";
+
+export default function Home() {
+  return <TransactionTable />;
+}
